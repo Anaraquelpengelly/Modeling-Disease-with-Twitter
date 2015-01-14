a6d873b8d535893b127f28cc4c929759130325d6
Wesley Chen <wesleychen@college.harvard.edu>
2015-01-14 01:55:06 -0500

    day 1 of tests

--- DataFiltering.py
+++ DataFiltering.py
@@ -1,31 +1,37 @@
 import pymongo
-import datetime
 
-# Connect to client
-client = pymongo.MongoClient('mongo-clusterAA5/twitter')'
+# how to connect to "mongo mongo-clusterAA5/twitter"
+client = pymongo.MongoClient()
 db = client.test
 
 # Enter as if we had run "mongo mongo-clusterAA5/twitter"
 db.setSlaveOk(true)
-db.geoTweets.find(
+var tempTest = db.geoTweets.find(
     {
         t : {
-            $regex : /.*Harvard.*/i
+            $regex : /.*st.*/
         }
     }
 ).pretty()
 
+# sort by date
+db.geoTweets.find().sort({"cr":-1})
 
-# how to subset and save the results of this
-db.geoPRTweets = db.geoTweets.find(
-   {
-     loc: {
-       $geoWithin: {
-          $geometry: {
-             type : "Box" ,
-             coordinates: [ [ [ 0, 0 ], [ 3, 6 ], [ 6, 1 ], [ 0, 0 ] ] ]
-          }
-       }
-     }
-   }
+
+# pln, plt order where ln runs north to south and thus has increments of W/E
+# lower left bound is 68W, 17.5N, top right bound is 65W, 18.7N
+# from http://docs.mongodb.org/manual/reference/operator/query/geoWithin/
+var prTweets = db.geoTweets.find( 
+    {
+    loc: {
+        $geoWithin: {
+            $box: [
+                [-68, 17.5],
+                [-65, 18.7]
+            ]
+        }
+    }
 )
+
+
+# TODO:     create a 2dsphere index as per http://docs.mongodb.org/manual/tutorial/build-a-2dsphere-index/
