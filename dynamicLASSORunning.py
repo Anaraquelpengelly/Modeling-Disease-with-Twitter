6b9e84ffa0a3b7c49c81b9a4170e06d85da79f64
Wesley Chen <wesleychen@college.harvard.edu>
2015-02-24 14:26:04 -0500

    dynamic results but still going

--- dynamicLASSORunning.py
+++ dynamicLASSORunning.py
@@ -10,23 +10,21 @@ from sklearn import cross_validation
 
 # start 6/15 as first week with data
 start = 0
-size = 8
-end = start+size
+#size = 8
+#end = start+size
 
 # read in file
-numbersData = pd.read_csv('numbersLaggedFull.csv', index_col = 0)
+numbersData = pd.read_csv('numbersLagged.csv', index_col = 0)
 print "data" + str(numbersData.shape)
 # set up dynamic prediction list
 numbersResult = []
 # set up coef used list
 myCoefs = []
 
-for start in xrange(numbersData.shape[0]-size):
-	print start
-	end = start+size
-
+for end in xrange(3,numbersData.shape[0]):
+	print end
 	# use size rows before it to train
-	kf = cross_validation.KFold(size, n_folds = size, shuffle = True)
+	kf = cross_validation.KFold(end-start, n_folds = 3, shuffle = True)
 	lasso = LassoCV(cv=kf, n_jobs=-1, positive=True)
 	# features is up to last column, target in last column
 	lassofit = lasso.fit(numbersData.ix[start:end,:-1], numbersData.ix[start:end,-1])
@@ -36,7 +34,7 @@ for start in xrange(numbersData.shape[0]-size):
 	numbersResult.append(lassofit.predict(numbersData.ix[end,:-1]))
 
 #set up outfile
-outFile = open('dynamicLassoFull8.txt', 'w')
+outFile = open('dynamicLassoRunning.txt', 'w')
 
 # write headers
 outFile.write('Predicted\tReal\n')
@@ -44,11 +42,11 @@ outFile.write('Predicted\tReal\n')
 for i in xrange(len(numbersResult)):
 	outFile.write(str(numbersResult[i]))
 	outFile.write("\t")
-	outFile.write(str(numbersData.ix[size+i,-1]))
+	outFile.write(str(numbersData.ix[i+3,-1]))
 	outFile.write("\n")
 
 # write coefficients over time starting from first widow
-coefOutFile = open('dynamicLassoFullCoef8.txt', 'w')
+coefOutFile = open('dynamicLassoRunningCoefs.txt', 'w')
 
 for i in xrange(numbersData.shape[1]):
 	coefOutFile.write(str(numbersData.columns[i]))
